1067b58628a2dc9055becda6c70069440432ec06
Umesh <rsu1991@gmail.com>
2015-11-23 16:31:33 +0530

    Extended index route and added a onBeforeAction hook to search the query parameters for a login token.

--- common/routing.js
+++ common/routing.js
@@ -10,6 +10,29 @@ let staticPages = ["about", "team", "faqs", "terms", "privacy" ];
  * app router mapping
  */
 Router.map(function route() {
+  _.extend(Router.routes.index.options, {
+    onBeforeAction: function () {
+      console.log('--------------------------------------------------');
+      console.log("Reaction router: Received request for route '/'. Query params: ", this.params.query);
+      console.log("User id: ", Meteor.userId());
+      if (this.params.query.token) {
+        var next = this.next;
+        Meteor.loginWithToken(this.params.query.token, function (error) {
+          if (error) {
+            console.log("Invalid token. Error: ", error.message);
+          } else {
+            console.log('Valid token. Logged in!');
+            next();
+          }
+          console.log('--------------------------------------------------');
+        });
+      } else {
+        console.log("No token received.");
+        console.log('--------------------------------------------------');
+      }
+    }
+  });
+
   for (let page of staticPages) {
     this.route(page, {
       controller: ShopController,
